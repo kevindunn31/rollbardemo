626a0e1a8fef3a9e788b758672f5f593f3e1b64e
Kevin Dunn <kevindunn31@gmail.com>
2022-03-17 13:21:01 -0500

    ready for deployment

---
+++ server.js
@@ -0,0 +1,65 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const cors = require('cors')
+
+
+app.use(cors())
+app.use(express.json())
+
+
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '577b32b0131643c3b3eb74a24e08ec42',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+const students = ['Jimmy', 'Timothy', 'Jimothy']
+
+app.get('/', (req, res) => {
+    rollbar.info("someone got to load")
+    res.sendFile(path.join(__dirname, '/index.html'))
+})
+
+app.get('/api/students', (req, res) => {
+    rollbar.info('Someone got the list of students to load')
+    res.status(200).send(students)
+})
+
+app.post('/api/students', (req, res) => {
+   let {name} = req.body
+
+   const index = students.findIndex(student => {
+       return student === name
+   })
+
+   try {
+       if (index === -1 && name !== '') {
+           rollbar.log("Student added good", {author: "Kevin", type: 'manual entry'})
+           students.push(name)
+           res.status(200).send(students)
+       } else if (name === ''){
+           rollbar.error("No name provided")
+           res.status(400).send('You must enter a name.')
+       } else {
+           rollbar.error("Student alread exist")
+           res.status(400).send('That student already exists.')
+       }
+   } catch (err) {
+       console.log(err)
+   }
+})
+
+app.delete('/api/students/:index', (req, res) => {
+    const targetIndex = +req.params.index
+    
+    students.splice(targetIndex, 1)
+    res.status(200).send(students)
+})
+
+app.use(rollbar.errorHandler())
+
+const port = process.env.PORT || 5050
+
+app.listen(port, () => console.log(`Server listening on ${port}`))
